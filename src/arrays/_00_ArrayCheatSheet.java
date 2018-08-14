74721aab3d90ab2e8622fa9531559c0d4272eaee
11000783 <league@iMac-2.attlocal.net>
2018-08-13 18:26:19 -0700

    ArraysCheatSheet, RobotRace(IP)

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -12,7 +12,7 @@ public class _00_ArrayCheatSheet {
 		names[3] = "ellie";
 		names[4] = "henery";
 		// 2. print the third element in the array
-		// System.out.println(names[names.length - 2]);
+ System.out.println(names[names.length - 2]);
 		// 3. set the third element to a different value
 
 		// 4. print the third element again
@@ -20,18 +20,23 @@ public class _00_ArrayCheatSheet {
 		// 5. use a for loop to print all the values in the array
 		// BE SURE TO USE THE ARRAY'S length VARIABLE
 		for (int i = 0; i < names.length; i++) {
-			System.out.println(names[i]);
+			// System.out.println(names[i]);
 
 		}
 		// 6. make an array of 50 integers
 		int ages[] = new int[50];
+		int smallest = 0;
 		// 7. use a for loop to make every value of the integer array a random number
 		Random rand = new Random();
 
 		for (int i = 0; i < ages.length; i++) {
 			ages[i] = rand.nextInt(50);
-			System.out.println(ages[i]);
+			if (ages[i] > smallest) {
+				smallest = ages[i];
+			}
+
 		}
+		System.out.println(smallest);
 		// 8. without printing the entire array, print only the smallest number in the
 		// array
 
